4b935a3e4d229e3ad037e02419b6afe264598f87
SrinivasKamadi <150056057+SrinivasKamadi@users.noreply.github.com>
2025-06-27 11:22:34 +0530

    added environment

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,35 +1,13 @@
 import axios from 'axios';
+const API_BASE_URL = import.meta.env.VITE_API_LOCAL_ENDPOINT
 
-const authClient = axios.create({
-  baseURL: 'https://micro-api-one.terraterri.com/api',
+
+const apiClient = axios.create({
+  baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${localStorage.getItem('adminToken')}` || null
   }
 });
 
-const masterClient = axios.create({
-  baseURL: 'https://micro-api-three.terraterri.com/api/',
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${localStorage.getItem('adminToken')}`
-  }
-});
-
-const projectClient = axios.create({
-  baseURL: 'https://micro-api-two.terraterri.com/api/project/',
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${localStorage.getItem('adminToken')}`
-  }
-});
-
-const expoClient = axios.create({
-  baseURL: 'https://mmworkspace.com/expo/api/',
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${localStorage.getItem('adminToken')}`
-  }
-});
-
-export { authClient, masterClient, projectClient, expoClient };
+export { apiClient };
